2be5183ef9a35962f6ffe054d841e3772f444acb
Drone <roxishan8@gmail.com>
2022-01-18 02:35:15 +0530

    DateItem component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-//use jsx attributes not class but className
+function ExpenseItem(props) {
+
+    //use jsx attributes not class but className
     return (<div className="expense-item">
-        <h1>
-            March 28
-        </h1>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>Car insuarance</h2>
-            <div className="expense-item__price">298</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>);
 }

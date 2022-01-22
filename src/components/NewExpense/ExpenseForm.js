34703431bbcb8a25086225796a7e0bf165ca58f4
Drone <roxishan8@gmail.com>
2022-01-22 12:42:13 +0530

    clear data after submit added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -68,27 +68,32 @@ const ExpenseForm = () => {
     const formSubmitHandler = (event) => {
         event.preventDefault();
         console.log(enteredValues);
+        setEnteredValues({
+            enteredTitle: '',
+            enteredDate: '',
+            enteredAmount: ''
+        });
     }
-    return <form onSubmit={formSubmitHandler }>
+    return <form onSubmit={formSubmitHandler}>
         <div className="new-expense__controls">
             {enteredValues.enteredTitle}
             <div className="new-expense__control">
                 <label>
                     Title
                 </label>
-                <input type='text' onChange={titleHandler}></input>
+                <input value={enteredValues.enteredTitle} type='text' onChange={titleHandler}></input>
             </div>
             <div className="new-expense__control">
                 <label>
                     Amount
                 </label>
-                <input type='number' min='0.01' step='0.01' onChange={amountHandler} />
+                <input value={enteredValues.enteredAmount} type='number' min='0.01' step='0.01' onChange={amountHandler} />
             </div>
             <div className="new-expense__control">
                 <label>
                     Date
                 </label>
-                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateHandler} />
+                <input value={enteredValues.enteredDate} type='date' min='2019-01-01' max='2022-12-31' onChange={dateHandler} />
             </div>
             <div className="new-expnse__actions">
                 <button type="submit">Add Expense</button>
